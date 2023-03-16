f335bedfe561e26aebfbc0c5a522dbc0c2b4c982
Sanjeet Sahay <sahays@amazon.com>
2023-03-15 23:42:39 -0700

    removed comments

--- gh-queries.js
+++ gh-queries.js
@@ -1,16 +1,3 @@
-/**
- * queries
- * repos: is%3Apublic+sort%3Aupdated-desc+type%3Arepository+org%3Apartiql&type=Repositories
- * opened issues: is:issue is:public created:2023-02-27..2023-03-10 user:partiql
- * closed issues: is:issue is:public closed:2023-02-27..2023-03-10 user:partiql
- * opened prs: is:pr is:public created:2023-02-27..2023-03-10 user:partiql
- * merged prs: is:pr is:public merged:2023-02-27..2023-03-10 user:partiql
- * closed prs: is:pr is:public closed:2023-02-27..2023-03-10 -merged:2023-02-27..2023-03-10 user:partiql
- * authored prs: is:pr author:jpschorr created:2022-10-01..2022-12-31
- * reviewed prs: is:pr reviewed-by:jpschorr -author:jpschorr created:2022-10-01..2022-12-31
- *
- */
-
 export const openedIssues = (from, to) => {
 	return encodeURIComponent(
 		`is:issue is:public created:${from}..${to} user:partiql`
